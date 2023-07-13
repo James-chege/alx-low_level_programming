89210e7b3e558fd9697109c061c1a5e6fe68a3d7
James-chege <chegejames11.cj@gmail.com>
2023-07-13 13:38:53 -0700

    Printing using the printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-int main(void) {
+/**
+ * main - starting point
+ *Description: Printing a line using printf function
+ *return: 0 (Success)
+ */
+int main(void)
+{
 printf("with proper grammar, but the outcome is a piece of art\n")
-	return (0);
+return (0);
 }
