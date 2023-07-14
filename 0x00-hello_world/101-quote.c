6e05f6918147f12866694d3669bfba9a8c5a40a9
James-chege <chegejames11.cj@gmail.com>
2023-07-14 00:17:59 -0700

    Printing a statement with to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<unistd.h>
+/**
+ * main - Printing of a given statement
+ * followed by a neew line to the standard error
+ *
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (0);
+}
