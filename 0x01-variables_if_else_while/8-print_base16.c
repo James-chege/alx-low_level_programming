f0e71436e8d85d327585574b61daeb958461d68d
James-chege <chegejames11.cj@gmail.com>
2023-07-16 01:31:14 -0700

    Loops printing hexadecimals

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - Printing hexadecimals usinguthcar function
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n;
+	char z;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n);
+	}
+	for (z = 'a'; z <= 'f'; z++)
+	{
+		putchar(z);
+	}
+	putchar('\n');
+	return (0);
+}
