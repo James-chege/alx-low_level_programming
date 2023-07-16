952a4b63c462b7a9fe5d5dc67ffcf42bf8113cbb
James-chege <chegejames11.cj@gmail.com>
2023-07-16 03:50:38 -0700

    Printing a sequence of numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+* main - Printing a sequence of numbers
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 9 ; a++)
+	{
+		for (b = a + 1; b < 10; b++)
+		{
+			putchar((a % 10) + '0');
+			putchar((b % 10) + '0');
+		
+		putchar(',');
+		putchar(' ');
+		}
+	}
+		putchar('\n');
+
+		return (0);
+}
