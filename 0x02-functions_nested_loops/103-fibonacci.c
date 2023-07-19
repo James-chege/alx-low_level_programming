8c3b8e76fd9ac14148f2af05c7dad55e6438379e
James-chege <chegejames11.cj@gmail.com>
2023-07-19 14:54:53 +0300

    Finding and prinitng first 98 fibonacci numbers

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Generating Fobanacci sequence
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+	long j = 1, k = 2, sum = k;
+	while (k + j < 4000000)
+	{
+	k += j;
+	if (k % 2 == 0)
+	sum += k;
+	++1;
+	}
+	printf("%ld\n", sum);
+
+	return (0);
+}
