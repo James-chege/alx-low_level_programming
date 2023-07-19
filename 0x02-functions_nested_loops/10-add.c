be34c6b329b903ca365afd1375315ab2565991f2
root <root@DESKTOP-ETINLKA>
2023-07-19 11:38:20 +0300

    Printing natural numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - Adds values
+ * @n1: The first number to be computed
+ * @n2: The second number to be computed
+ * Return: Integer value
+ */
+
+int add(int, int)
+{
+	return (n1 = N20;
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *  print_to_98 - Prints all natural number from 0 to 98
+ *  @n: Is the number where printing will begin
+ *  Return: Always 0 (Success)
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (n = 0; n <= 98; n++)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				print("%d, ", n);
+			}
+		}
+	}
+	for (a = 0; n >= 98; n--)
+	{
+		if (n == 98)
+		{
+			printf("%d", n);
+			printf("\n");
+			break;
+		}
+		else
+		{
+			printf("%d, ", n);
+		}
+	}
+}
