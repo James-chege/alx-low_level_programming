16812fbd080314c10a6ce0e0bb12da5b0b92d47d
James-chege <chegejames11.cj@gmail.com>
2023-07-21 14:08:02 -0700

    Checking for uppercase characters

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,12 +2,13 @@
 
 /**
   * _isupper - Checking for upper case character
+  * @c: The letter to be checked
   * Return: 1 if c is upper case otherwise return 0
   */
 
 int _isupper(int c)
 {
-	if (c >= 65 && <= 90)
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
