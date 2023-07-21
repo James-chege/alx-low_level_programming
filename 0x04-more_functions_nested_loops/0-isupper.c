48852b3c9d298b1ed8c8520b8d33dd0819461d5b
James-chege <chegejames11.cj@gmail.com>
2023-07-21 05:46:26 -0700

    more functions

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * _isupper - Checking for upper case character
+  * Return: 1 if c is upper case otherwise return 0
+  */
+
+int _isupper(int c)
+{
+	int c = 'C', 'c';
+
+	c = 'C';
+	_putchar('1');
+	c = 'a';
+	_putchar('0');
+	return (0);
+
+}
