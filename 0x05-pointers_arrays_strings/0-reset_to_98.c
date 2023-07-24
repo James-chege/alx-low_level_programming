8d5c339e1e981bbc5b4adc8c9eace863a393ab7d
James-chege <chegejames11.cj@gmail.com>
2023-07-24 17:41:23 +0300

    Pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - prints prints an int function with an update
+ * @*n: Is the pointer variable to be updated
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	int *n;
+
+	*n = 98;
+	_putchar(*n);
+}
