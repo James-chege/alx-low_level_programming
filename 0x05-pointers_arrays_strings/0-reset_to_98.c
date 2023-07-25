77471715e429a49afe0c4378c33cce68a690d1c8
James-chege <chegejames11.cj@gmail.com>
2023-07-25 02:51:42 -0700

    Updating a value that a pointer points

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,14 +2,12 @@
 
 /**
  * reset_to_98 - prints prints an int function with an update
- * @*n: Is the pointer variable to be updated
+ * @n: Is the pointer variable to be updated
  * Return: Always 0
  */
 
 void reset_to_98(int *n)
 {
-	int *n;
+	 *n = 98;
 
-	*n = 98;
-	_putchar(*n);
 }
