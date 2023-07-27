42e22fc69d40a156445546f39674c1b6379046a4
James-chege <chegejames11.cj@gmail.com>
2023-07-27 07:30:22 -0700

    Pointers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -34,8 +34,8 @@ int main(void)
 	half1 += 1;
 	half2 %= 10000000000;
 	}
-	printf("%lu, %lu", half1, half2);
-	if (count != 99);
+	printf("%lu%lu", half1, half2);
+	if (count != 98);
 	printf(", ");
 	fib1_half1 = fib2_half1;
 	fib1_half2 = fib2_half2;
---
+++ README.md
@@ -0,0 +1,3 @@
+alx-low_level_programming
+Variables, if, else, while
+Working on nested loops
