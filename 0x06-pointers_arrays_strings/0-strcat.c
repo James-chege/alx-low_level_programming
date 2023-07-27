dfa8055d149c8f7702412f670d44937f7df277f6
James-chege <chegejames11.cj@gmail.com>
2023-07-27 13:35:06 -0700

    Concatenating two lines

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,28 +1,29 @@
 #include "main.h"
-
 /**
- * _strcat - Prints a concentration of two strings
- * @dest: String one to be concentrated
- * @src: Second string to be concentrated
- * Return: O
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ *
+ * Return: void
  */
-
 char *_strcat(char *dest, char *src)
 {
 	int i;
-	intj;
+	int j;
 
 	i = 0;
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
+	j = 0;
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
-		i++;
+		i++;;
 		j++;
 	}
+
 	dest[i] = '\0';
 	return (dest);
 }
